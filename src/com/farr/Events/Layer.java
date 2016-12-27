9a6e7eea9f5f77501bada4c78b8db0d4d64f1781
Henry Farr <hfarr80@yahoo.com>
2016-12-26 21:02:13 -0500

    I think I moved the location of this file

--- src/com/farr/Events/Layer.java
+++ src/com/farr/Events/Layer.java
@@ -4,14 +4,13 @@ import java.awt.Graphics;
 import java.util.List;
 
 public interface Layer {
-	
+		
 	public void render(Graphics g);
 	
 	public void update();
 
 	public void onEvent(Event e);
 	
-	//TODO change implementation of Layer and BlockingLayer to figure out how we really want init used
-	public void init(List<BlockingLayer> l);
+	public void init(List<Layer> l);
 	
 }
